b260798d9d71a290119e8b9b66e8fe5ca47ad9b0
DanielBonnery <daniel.bonnery@gmail.com>
2016-05-16 13:26:15 -0400

    a

--- R/plots.R
+++
@@ -1,5 +0,0 @@
-data(crop,package="dataCountyCropSurveySatellite")
-attach(crop)
-library(ggplot2)
-plot1<-ggplot(data = crop,aes(x=crnpxel,y=crnsrvy,group=county,color=county,linetype=county))+geom_line()+geom_point()+xlab("Pixels")+ylab("Hectares")
-plot2<-ggplot(data = crop,aes(x=soypxel,y=soysrvy,group=county,color=county,linetype=county))+geom_line()+geom_point()+xlab("Pixels")+ylab("Hectares")
